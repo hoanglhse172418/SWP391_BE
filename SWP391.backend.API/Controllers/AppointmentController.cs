2a7d4313273bb7c6f1183ed594e4d5a73f54a44d
fishbone2710 <khanhvtse150420@fpt.edu.vn>
2025-03-19 11:24:30 +0700

    fix return wrong number code

--- SWP391.backend.API/Controllers/AppointmentController.cs
+++ SWP391.backend.API/Controllers/AppointmentController.cs
@@ -145,11 +145,11 @@ namespace SWP391.backend.api.Controllers
 
             return result switch
             {
-                -1 => Ok("This appointment already had payment. Update processStep for appointment to Waiting Inject"),
                 0 => NotFound("Appointment not found"),
                 1 => Ok("Updated appointment with Single type successfully!"),
                 2 => Ok("Payment already exists. Appointment with Package type updated successfully!"),
                 3 => Ok("New payment created. Appointment with Package type updated successfully!"),
+                4 => Ok("This appointment already had payment. Update processStep for appointment to Waiting Inject"),
                 _ => BadRequest("Unknown error occurred")
             };
         }
